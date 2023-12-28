cd6b1577f3d3b6f680e6108037e2e1dfcac7e9c2
Roman Sachuck <roman.sachuk.1997@mail.ru>
2023-12-28 17:48:26 +0300

    .

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+
+            users = ...;// Получаем список пользователей.
+
+            for (int i = 0; i < users.Count; i++) 
+            {
+                Console.WriteLine($"Здравствуйте {users[i].Name}");
+
+                if (!users[i].IsPremium)
+                    ShowAds();
+            }
+        }
+    }
+}
